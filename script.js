cb56413e61f142bd456a5635209107170a964002
mokumoku-chan <maho.kumo1997@gmail.com>
2022-05-17 00:18:04 +0900

    Edit script.js display line

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(function(){
       url: "http://express.heartrails.com/api/json?method=getStations&name=" + $('#station').val(),
       dataType : 'jsonp',
     }).done(function(data){
-      $('#line').text(data.response.station.line);
+      $('#line').text(data.response.station[0].line);
     }).fail(function(data){
       alert('通信に失敗しました。')
     });
