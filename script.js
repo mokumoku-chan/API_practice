0caa4cd41113386282538a0be63b9de6f1c8393e
mokumoku-chan <maho.kumo1997@gmail.com>
2022-05-20 01:14:11 +0900

    Edit script.js extract all lines to log

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 $(function(){
   $('#btn').on('click', function() {
-    alert($('#station').val());
     $.ajax({
       url: "http://express.heartrails.com/api/json?method=getStations&name=" + $('#station').val(),
       dataType : 'jsonp',
     }).done(function(data){
-      $('#line').text(data.response.station[0].line);
+      console.log(data.response.station.length);
+      for(var i = 0; i < data.response.station.length; i++){
+        console.log(data.response.station[i].line);
+      }
+        $('#line').text(data.response.station[0].line);
     }).fail(function(data){
       alert('通信に失敗しました。')
     });
